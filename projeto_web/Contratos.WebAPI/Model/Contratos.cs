65026292a87114c8d5ff6b88cb2735ff12c9cf8d
Daniel Torres <danielteodolino@gmail.com>
2021-02-22 23:53:43 -0300

    inclusão do primeiro model Contratos.cs - inicialmente com 6 propriedades

---
+++ projeto_web/Contratos.WebAPI/Model/Contratos.cs
@@ -0,0 +1,12 @@
+namespace Contratos.WebAPI.Model
+{
+    public class Contratos
+    {
+        public int ContratoID { get; set; }
+        public string DataInicioVigencia { get; set; }
+        public string DataFimVigencia { get; set; }
+        public string ProcessoTCE { get; set; }
+        public string LinkRedmine { get; set; }
+        public string ObjetoAcordo { get; set; }     
+    }
+}
