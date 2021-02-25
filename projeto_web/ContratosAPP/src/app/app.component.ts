96f4a962004593a58dbd48655332c630b67551db
Daniel Torres <danielteodolino@gmail.com>
2021-02-25 15:19:03 -0300

    iniciando configuração do front-end (angular)

--- projeto_web/ContratosAPP/src/app/app.component.ts
+++ projeto_web/ContratosAPP/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ContratosAPP';
+  title = 'Gerencimento de Contratos';
 }
