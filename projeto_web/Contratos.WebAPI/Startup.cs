2f74dd6fd2f3c061f4f9804a8b18e5b06d0e5804
Daniel Torres <danielteodolino@gmail.com>
2021-03-02 22:12:46 -0300

    organização das camadas (solução, Domínio e Repositório) e referenciando o projeto

--- projeto_web/Contratos.WebAPI/Startup.cs
+++ projeto_web/Contratos.WebAPI/Startup.cs
@@ -36,12 +36,14 @@ namespace Contratos.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            
 
             app.UseHttpsRedirection();
 
